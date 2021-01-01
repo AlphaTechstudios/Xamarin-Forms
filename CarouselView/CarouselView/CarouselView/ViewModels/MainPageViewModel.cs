0a2275adb3535dccc972cc58935f347d61921f61
Brahim Guerbouj <MobileApplications@outlook.fr>
2021-01-01 13:54:14 +0100

    Xamarin Forms - Carousel View

---
+++ CarouselView/CarouselView/CarouselView/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Prism.Navigation;
+using System.Collections.Generic;
+
+namespace CarouselView.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private IEnumerable<string> imagesList;
+        public IEnumerable<string> ImagesList 
+        { 
+            get => imagesList;
+            set => SetProperty(ref imagesList, value);
+        }
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Carousel View";
+        }
+
+        public override void Initialize(INavigationParameters parameters)
+        {
+            ImagesList = new List<string>
+            {
+                "https://images.pexels.com/photos/162520/farmer-man-shepherd-dog-162520.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+                "https://images.pexels.com/photos/1069712/pexels-photo-1069712.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+                "https://images.pexels.com/photos/3551498/pexels-photo-3551498.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+                "https://images.pexels.com/photos/3651618/pexels-photo-3651618.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            };
+        }
+
+
+    }
+}
---
+++ CarouselView/CarouselView/CarouselView/Views/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿
+namespace CarouselView.Views
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
